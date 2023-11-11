ae1622ffc8f65736a6a22b2441f3bf19ad75c7ff
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-11-11 16:44:48 +0300

    Sdelano, nado dobavit commentaries)) and vozmojno pomenyat realizaciy

--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	arr1 := []int{2, 4, 6, 8, 10}
+
+	for i := 0; i < len(arr1); i++ {
+		go func(i int) {
+			fmt.Println(arr1[i] * arr1[i])
+		}(i)
+	}
+
+	time.Sleep(100 * time.Millisecond)
 }
