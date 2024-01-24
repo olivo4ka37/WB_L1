efd1a13a6f980de88640f1957ff3f5cdd2e98303
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-01-24 16:39:47 +0300

    SDELANO

--- 14/main.go
+++ 14/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	// Примеры различных переменных
+	var structVariable struct{}
+	var integerVariable int = 42
+	var floatVariable float64 = 3.14
+	var stringVariable string = "Hello, Golang!"
+	var boolVariable bool = true
+
+	// Определение типа переменной
+	detectType(structVariable)
+	detectType(integerVariable)
+	detectType(floatVariable)
+	detectType(stringVariable)
+	detectType(boolVariable)
+}
+
+// Функция для определения типа переменной
+func detectType(variable interface{}) {
+	// Используем reflect.TypeOf для определения типа переменной
+	variableType := reflect.TypeOf(variable)
+	fmt.Printf("Тип переменной: %v\n", variableType)
 }
