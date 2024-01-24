c86f21957d6f860e13a591f4c3af8cf76c82e7d2
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-01-24 16:17:40 +0300

    SDELANO

--- 12/main.go
+++ 12/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Массив строк
+	xArray := []string{"cat", "cat", "dog", "cat", "tree"}
+	// Множество значений string
+	stringSet := make(map[string]struct{})
+
+	// Проверяем наличие значений из xArray в stringSet, если нету записываем в stringSet
+	for _, x := range xArray {
+		_, contains := stringSet[x] //contains - булевое значение определяющее наличие данного ключа в множестве
+		if !contains {
+			stringSet[x] = struct{}{}
+		}
+	}
+
+	fmt.Println(stringSet)
 }
