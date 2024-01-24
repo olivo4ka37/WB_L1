2925289e94df225675f9868e6bde9e3dc7b7dffd
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-01-24 16:20:15 +0300

    SDELANO

--- 13/main.go
+++ 13/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x1, x2 := 1, 2
+	fmt.Println("x1 is:", x1, "x2 is:", x2)
+	x1, x2 = x2, x1
+	fmt.Println("x1 is:", x1, "x2 is:", x2)
 }
