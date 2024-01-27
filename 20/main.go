b5c9544bdc43cf399002e2becd25a1676b46da8a
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-01-27 17:09:36 +0300

    SDELANO

--- 20/main.go
+++ 20/main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fmt.Println(reverseWords("snow    dog sun"))
+}
+
+// reverseWords пишет слова из строки в обратном порядке, игнорирует мультипробелы
+func reverseWords(s string) string {
+	result, word := "", ""
+
+	for i := 0; i < len(s); i++ {
+		// i== len(s)-1 Проверяет конецу строки, для того чтобы добавить последнее слово.
+		if i == len(s)-1 {
+			// Проверяет необходимость для того чтобы добавить последний символ в переменную word.
+			if string(s[i]) != " " {
+				word += string(s[i])
+			}
+			result = word + result
+
+			// string(s[i]) == " " && string(s[i+1]) != " " && word != "" Проверяет на пробелы между словами,
+			// игнорирует мультипробелы.
+			// После всех проверок добовляет символ если он у нас есть.
+		} else if string(s[i]) == " " && string(s[i+1]) != " " && word != "" {
+			result = " " + word + result
+			word = ""
+		} else if string(s[i]) != " " {
+			word += string(s[i])
+		}
+	}
+
+	return result
 }
