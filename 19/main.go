d5da00e4e6c56d048c9ae8499ca1f0a3d1d11a37
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-01-27 16:39:41 +0300

    SDELANO

--- 19/main.go
+++ 19/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// Инициализируем стэк структуру и тестовую строку
+	s := stack{}
+	testString := "главрыба_что-то_на_японском_лол)_ ぁ さ ゎ デ ニ ね"
+
+	// Записываем все руны из строки в стэк
+	for _, x := range testString {
+		s.push(x)
+	}
+
+	// Достаем из стэка все что мы туда занесли
+	// Тем самым получаем обратную строку
+	for range testString {
+		fmt.Print(string(s.pop()))
+	}
+
+}
+
+// стэк структура
+type stack struct {
+	items []rune
+}
+
+// push Записываем значение в стэк
+func (s *stack) push(i rune) {
+	s.items = append(s.items, i)
+}
+
+// pop достаем значение из стэка
+func (s *stack) pop() rune {
+	l := len(s.items) - 1
+	lastItem := s.items[l]
+	s.items = s.items[:l]
+	return lastItem
 }
